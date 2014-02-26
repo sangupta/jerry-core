feab1ecfe08113cde955c56190ca9c70b7ed8221
Sandeep Gupta <sandy.pec@gmail.com>
2014-02-26 17:37:30 +0530

    Added constant file for HTTP MIME types for common response codes

---
+++ src/main/java/com/sangupta/jerry/constants/HttpMimeType.java
@@ -0,0 +1,63 @@
+/**
+ *
+ * jerry - Common Java Functionality
+ * Copyright (c) 2012, Sandeep Gupta
+ * 
+ * http://www.sangupta/projects/jerry
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * 		http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ */
+
+package com.sangupta.jerry.constants;
+
+/**
+ * Constants regarding various HTTP MIME codes. The basic values have been copied from
+ * http://en.wikipedia.org/wiki/Internet_media_type
+ * 
+ * Additional codes have been copied from the Wikipedia page at,
+ * http://www.iana.org/assignments/media-types/media-types.xhtml
+ * 
+ * @author sangupta
+ *
+ */
+public class HttpMimeType {
+
+	public static final String GIF_IMAGE = "image/gif";
+	
+	public static final String PNG_IMAGE = "image/png";
+	
+	public static final String JPEG_IMAGE = "image/jpeg";
+	
+	public static final String JPG_IMAGE = "image/jpg";
+	
+	public static final String SVG_IMAGE = "image/svg+xml";
+	
+	public static final String TEXT_PLAIN = "text/plain";
+	
+	public static final String CSS = "text/css";
+	
+	public static final String HTML = "text/html";
+	
+	public static final String JAVASCRIPT = "text/javascript";
+	
+	public static final String RTF = "text/rtf";
+	
+	public static final String XML = "text/xml";
+	
+	public static final String JSON = "application/json";
+	
+	public static final String ATOM = "application/atom+xml";
+	
+	public static final String PDF = "application/pdf";
+}
