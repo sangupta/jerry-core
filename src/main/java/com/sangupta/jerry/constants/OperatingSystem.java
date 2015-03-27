4002656881874f0d1e4063a2dddaa2929d426342
Sandeep Gupta <sandy.pec@gmail.com>
2015-03-28 00:13:15 +0530

    Added enum for major operating systems

---
+++ src/main/java/com/sangupta/jerry/constants/OperatingSystem.java
@@ -0,0 +1,60 @@
+/**
+ *
+ * jerry - Common Java Functionality
+ * Copyright (c) 2012-2015, Sandeep Gupta
+ * 
+ * http://sangupta.com/projects/jerry
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * 		http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ */
+ 
+package com.sangupta.jerry.constants;
+
+/**
+ * Enumeration listing all major operating systems that we can detect.
+ * 
+ * @author sangupta
+ *
+ */
+public enum OperatingSystem {
+	
+	AIX,
+	
+	FreeBSD,
+	
+	HP_UX,
+	
+	OS_400,
+	
+	OS_2,
+	
+	Irix,
+	
+	Windows,
+	
+	Mac,
+	
+	MacOSX,
+	
+	NetBSD,
+	
+	Linux,
+	
+	SunOS,
+	
+	Solaris,
+	
+	Unknown;
+
+}
