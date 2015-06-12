bea33e1717c01730e4586b409a6d08aea9e229d6
Sandeep Gupta <sandy.pec@gmail.com>
2015-06-13 00:59:32 +0530

    Moved jersey1 based GsonJsonProvider from jerry-web project

--- src/main/java/com/sangupta/jerry/jersey/GsonJsonProvider.java
+++ src/main/java/com/sangupta/jerry/jersey/GsonJsonProvider.java
@@ -1,4 +1,25 @@
-package com.sangupta.jerry.jersey2;
+/**
+ *
+ * jerry-web - Common Java Functionality
+ * Copyright (c) 2012-2014, Sandeep Gupta
+ * 
+ * http://sangupta.com/projects/jerry
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * 		http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ */
+
+package com.sangupta.jerry.jersey;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -16,11 +37,10 @@ import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.MultivaluedMap;
 import javax.ws.rs.ext.Provider;
 
-import org.glassfish.jersey.message.internal.AbstractMessageReaderWriterProvider;
-
 import com.google.gson.FieldNamingPolicy;
 import com.google.gson.Gson;
 import com.sangupta.jerry.util.GsonUtils;
+import com.sun.jersey.core.provider.AbstractMessageReaderWriterProvider;
 
 /**
  * JSON serialization mechanism for Jersey. We use Google GSON library
@@ -76,7 +96,6 @@ public class GsonJsonProvider extends AbstractMessageReaderWriterProvider<Object
         OutputStreamWriter outputStreamWriter = new OutputStreamWriter(stream, encoding);
         outputStreamWriter.write(json);
         outputStreamWriter.flush();
-        
     }
 
 }
