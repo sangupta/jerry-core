3cffb8abf27f450045237e0ad5251050b5f24a9e
Sandeep Gupta <sandy.pec@gmail.com>
2015-12-08 14:10:15 +0530

    Enable tests for compress/decompress via CompressionUtils

--- src/test/java/com/sangupta/jerry/util/TestCompressionUtils.java
+++ src/test/java/com/sangupta/jerry/util/TestCompressionUtils.java
@@ -16,30 +16,30 @@ public class TestCompressionUtils {
 	
 	private static final int MAX_RUNS = 1000 * 10;
 
-//	@Test
-//	public void testCompressDecompress() throws UnsupportedEncodingException {
-//		for(int index = 0; index < MAX_RUNS; index++) {
-//			String text = StringUtils.getRandomString(1000);
-//			
-//			byte[] bytes1 = CompressionUtils.compress(text);
-//			String textBack = CompressionUtils.uncompressToString(bytes1);
-//			Assert.assertEquals(text, textBack);
-//			
-//			// other methods
-//			byte[] bytes = text.getBytes(StringUtils.DEFAULT_CHARSET);
-//			bytes1 = CompressionUtils.compress(bytes);
-//			byte[] bytesBack = CompressionUtils.uncompress(bytes1);
-//			Assert.assertArrayEquals(bytes, bytesBack);
-//			
-//			bytes1 = CompressionUtils.compress(text, StringUtils.CHARSET_UTF8);
-//			textBack = CompressionUtils.uncompressToString(bytes1);
-//			Assert.assertEquals(text, textBack);
-//			
-//			bytes1 = CompressionUtils.compress(text, StringUtils.CHARSET_UTF8);
-//			textBack = CompressionUtils.uncompressToString(bytes1, StringUtils.CHARSET_UTF8);
-//			Assert.assertEquals(text, textBack);
-//		}
-//	}
+	@Test
+	public void testCompressDecompress() throws UnsupportedEncodingException {
+		for(int index = 0; index < MAX_RUNS; index++) {
+			String text = StringUtils.getRandomString(1000);
+			
+			byte[] bytes1 = CompressionUtils.compress(text);
+			String textBack = CompressionUtils.uncompressToString(bytes1);
+			Assert.assertEquals(text, textBack);
+			
+			// other methods
+			byte[] bytes = text.getBytes(StringUtils.DEFAULT_CHARSET);
+			bytes1 = CompressionUtils.compress(bytes);
+			byte[] bytesBack = CompressionUtils.uncompress(bytes1);
+			Assert.assertArrayEquals(bytes, bytesBack);
+			
+			bytes1 = CompressionUtils.compress(text, StringUtils.CHARSET_UTF8);
+			textBack = CompressionUtils.uncompressToString(bytes1);
+			Assert.assertEquals(text, textBack);
+			
+			bytes1 = CompressionUtils.compress(text, StringUtils.CHARSET_UTF8);
+			textBack = CompressionUtils.uncompressToString(bytes1, StringUtils.CHARSET_UTF8);
+			Assert.assertEquals(text, textBack);
+		}
+	}
 	
 	@Test
 	public void testGzip() throws IOException {
