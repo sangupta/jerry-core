98e96093756daebc02d7b988c2687e794f43c2dc
Sandeep Gupta <sandy.pec@gmail.com>
2015-12-22 12:51:59 +0530

    Test convenience methods in AbstractUserLocalStore

---
+++ src/test/java/com/sangupta/jerry/store/TestAbstractUserLocalStore.java
@@ -0,0 +1,37 @@
+package com.sangupta.jerry.store;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Unit tests for {@link AbstractUserLocalStore}
+ * 
+ * @author sangupta
+ *
+ */
+public class TestAbstractUserLocalStore {
+
+	@Test
+	public void testCommonMethods() {
+		UserLocalStore store = new InMemoryUserLocalStore();
+		
+		// when store is empty
+		Assert.assertEquals(true, store.getBoolean("bool", true));
+		Assert.assertEquals(63, store.getInt("int", 63));
+		Assert.assertEquals(63l, store.getLong("long", 63l));
+		Assert.assertEquals(63f, store.getFloat("float", 63f), 0f);
+		Assert.assertEquals(63d, store.getDouble("double", 63d), 0d);
+		
+		// when store has values
+		store.put("bool", false);
+		store.put("int", 23);
+		store.put("long", 23l);
+		store.put("float", 23f);
+		store.put("double", 23d);
+		Assert.assertEquals(false, store.getBoolean("bool", false));
+		Assert.assertEquals(23, store.getInt("int", 63));
+		Assert.assertEquals(23l, store.getLong("long", 63l));
+		Assert.assertEquals(23f, store.getFloat("float", 63f), 0f);
+		Assert.assertEquals(23d, store.getDouble("double", 63d), 0d);
+	}
+}
