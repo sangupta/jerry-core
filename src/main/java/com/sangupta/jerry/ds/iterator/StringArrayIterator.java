2ffbda5c6967a74b1560cfbbc95e92695e890904
Sandeep Gupta <sandy.pec@gmail.com>
2016-12-07 14:19:12 +0530

    Added @NotThreadSafe annotation and added java-docs

--- src/main/java/com/sangupta/jerry/ds/iterator/StringArrayIterator.java
+++ src/main/java/com/sangupta/jerry/ds/iterator/StringArrayIterator.java
@@ -23,16 +23,47 @@ package com.sangupta.jerry.ds.iterator;
 
 import java.util.Iterator;
 
+import net.jcip.annotations.NotThreadSafe;
+
+/**
+ * An iterator that works over a <code>String[]</code> (String array).
+ * 
+ * @author sangupta
+ *
+ */
+@NotThreadSafe
 public class StringArrayIterator implements Iterator<String> {
     
+	/**
+	 * The backing array
+	 */
     private final String[] items;
     
+    /**
+     * The current position in the array
+     */
     private int position = 0;
     
+    /**
+     * Construct an iterator over the given backing array
+     * 
+     * @param items the backing array to use
+     * 
+     */
     public StringArrayIterator(String[] items) {
+    	if(items == null) {
+    		throw new IllegalArgumentException("Backing string array cannot be null");
+    	}
+    	
         this.items = items;
     }
     
+    /**
+     * Get the current position in the backing string array.
+     * 
+     * @return the index position in the backing array
+     * 
+     */
     public int getPosition() {
     	return this.position;
     }
@@ -55,11 +86,21 @@ public class StringArrayIterator implements Iterator<String> {
         return false;
     }
 
+    /**
+     * Get the next element from the array.
+     * 
+     */
     @Override
     public String next() {
         return this.items[this.position++];
     }
     
+    /**
+	 * Peek the next element without removing it.
+	 * 
+	 * @return the next element if available, or <code>null</code>.
+	 * 
+	 */
     public String peek() {
         if(!this.hasNext()) {
             return null;
