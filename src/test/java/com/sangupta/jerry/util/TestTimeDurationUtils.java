7c4c1510746009aae4f1b9b7c199515f29ec1544
Sandeep Gupta <sandy.pec@gmail.com>
2017-01-10 15:47:58 +0530

    More tests

--- src/test/java/com/sangupta/jerry/util/TestTimeDurationUtils.java
+++ src/test/java/com/sangupta/jerry/util/TestTimeDurationUtils.java
@@ -35,6 +35,76 @@ public class TestTimeDurationUtils {
 
 	@Test
 	public void testAgo() {
-		Assert.assertEquals("less than a minute ago", TimeDurationUtils.ago(System.currentTimeMillis()));
+		long time = System.currentTimeMillis();
+		
+		Assert.assertEquals("less than a minute ago", TimeDurationUtils.ago(time));
+		
+		time -= DateUtils.ONE_MINUTE;
+		Assert.assertEquals("about a minute ago", TimeDurationUtils.ago(time));
+		
+		time -= (3 * DateUtils.ONE_MINUTE);
+		Assert.assertEquals("about 4 minutes ago", TimeDurationUtils.ago(time));
+		
+		time -= (56 * DateUtils.ONE_MINUTE);
+		Assert.assertEquals("about an hour ago", TimeDurationUtils.ago(time));
+		
+		time -= DateUtils.ONE_HOUR;
+		Assert.assertEquals("about 2 hours ago", TimeDurationUtils.ago(time));
+		
+		time -= (22 * DateUtils.ONE_HOUR);
+		Assert.assertEquals("about a day ago", TimeDurationUtils.ago(time));
+		
+		time -= (2 * DateUtils.ONE_DAY);
+		Assert.assertEquals("about 3 days ago", TimeDurationUtils.ago(time));
+		
+		time -= (27 * DateUtils.ONE_DAY);
+		Assert.assertEquals("about a month ago", TimeDurationUtils.ago(time));
+		
+		time -= (2 * DateUtils.ONE_MONTH);
+		Assert.assertEquals("about 3 months ago", TimeDurationUtils.ago(time));
+		
+		time -= (10 * DateUtils.ONE_MONTH);
+		Assert.assertEquals("about an year ago", TimeDurationUtils.ago(time));
+		
+		time -= (2 * DateUtils.ONE_YEAR);
+		Assert.assertEquals("about 3 years ago", TimeDurationUtils.ago(time));
 	}
+	
+	@Test
+	public void testAgoCondensed() {
+		long time = System.currentTimeMillis();
+		
+		Assert.assertEquals("now", TimeDurationUtils.ago(time, true));
+		
+		time -= DateUtils.ONE_MINUTE;
+		Assert.assertEquals("1min", TimeDurationUtils.ago(time, true));
+		
+		time -= (3 * DateUtils.ONE_MINUTE);
+		Assert.assertEquals("4min", TimeDurationUtils.ago(time, true));
+		
+		time -= (56 * DateUtils.ONE_MINUTE);
+		Assert.assertEquals("1h", TimeDurationUtils.ago(time, true));
+		
+		time -= DateUtils.ONE_HOUR;
+		Assert.assertEquals("2h", TimeDurationUtils.ago(time, true));
+		
+		time -= (22 * DateUtils.ONE_HOUR);
+		Assert.assertEquals("1d", TimeDurationUtils.ago(time, true));
+		
+		time -= (2 * DateUtils.ONE_DAY);
+		Assert.assertEquals("3d", TimeDurationUtils.ago(time, true));
+		
+		time -= (27 * DateUtils.ONE_DAY);
+		Assert.assertEquals("1mo", TimeDurationUtils.ago(time, true));
+		
+		time -= (2 * DateUtils.ONE_MONTH);
+		Assert.assertEquals("3mo", TimeDurationUtils.ago(time, true));
+		
+		time -= (10 * DateUtils.ONE_MONTH);
+		Assert.assertEquals("1y", TimeDurationUtils.ago(time, true));
+		
+		time -= (2 * DateUtils.ONE_YEAR);
+		Assert.assertEquals("3y", TimeDurationUtils.ago(time, true));
+	}
+
 }
